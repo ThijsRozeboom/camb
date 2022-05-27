fbf32afddc2b3ea888a7e9bf5c83cb04741e4044
ThijsRozeboom <106308340+ThijsRozeboom@users.noreply.github.com>
2022-05-27 10:28:20 +0200

    Update InitialPower.f90

--- fortran/InitialPower.f90
+++ fortran/InitialPower.f90
@@ -53,7 +53,7 @@
         real(dl) :: pivot_tensor = 0.05_dl
         real(dl) :: As = 1._dl
         real(dl) :: At = 1._dl !A_T at k_0_tensor if tensor_parameterization==tensor_param_AT
-        real(dl) :: Tr = 0._dl
+
         real(dl), private :: curv = 0._dl !curvature parameter
     contains
     procedure :: Init => TInitialPowerLaw_Init
@@ -142,7 +142,7 @@
     TInitialPowerLaw_ScalarPower = this%As * exp(lnrat * (this%ns - 1 + &
         &             lnrat * (this%nrun / 2 + this%nrunrun / 6 * lnrat)))
         
-    TInitialPowerLaw_ScalarPower = TInitialPowerLaw_ScalarPower(1+this%Tr*cos(lnrat*30))
+
 
     end function TInitialPowerLaw_ScalarPower
 
